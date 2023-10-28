11dd6a7191e13f1019d6b90ea33a523a8c572ccf
Jamal Watts <jwatts721@yahoo.com>
2023-10-28 10:23:53 -0400

    Added coment for unsafe fallback function demo

--- Ethereum Developer Masterclass/RemixIDE/contracts/fallback-functions-unsafe.sol
+++ Ethereum Developer Masterclass/RemixIDE/contracts/fallback-functions-unsafe.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
+/*
+This contract is not safe because it does not hae a fallback or receive function.
+If any funds get sent to the contract directly to the address without using the payToContract 
+or any other payable function, those funds will be lost in the blockchain FOREVER.
+*/
+
 contract FallbackDemo
 {
     uint256 public amount;
